2fa5af306212462a645c0e63736038d9282ec183
DeepankarVyas <deepankar.vyas@ucdconnect.ie>
2023-10-10 12:18:23 +0100

    Adding practical03

---
+++ practical03/print_ex.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+//declaring and initializing variables
+int i1=100;
+int i2=200;
+float f1=1.23;
+float f2=200.1234555;
+
+printf("the int values are %d,%d\nthe float values are %.2f,%.2f\n",i1,i2,f1,f2);
+
+return 0;
+}
